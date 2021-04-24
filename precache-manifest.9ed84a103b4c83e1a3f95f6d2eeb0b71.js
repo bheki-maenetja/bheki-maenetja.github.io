85cc720fc8efa43345a4841c74050aaee0ba39c7
bheki-maenetja <bhekimaenetja@gmail.com>
2021-04-24 19:33:57 +0100

    added files for portfolio site

---
+++ precache-manifest.9ed84a103b4c83e1a3f95f6d2eeb0b71.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1285914927acf6023533b38a3c4c72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7805dcfedd02fe2b0317",
+    "url": "/static/css/2.f408f721.chunk.css"
+  },
+  {
+    "revision": "0217ce9e39127cbe46bd",
+    "url": "/static/css/main.23471d30.chunk.css"
+  },
+  {
+    "revision": "7805dcfedd02fe2b0317",
+    "url": "/static/js/2.be156be7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.be156be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0217ce9e39127cbe46bd",
+    "url": "/static/js/main.37cde7a8.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "b9104dadba9841227bf9a95a3fbad2c6",
+    "url": "/static/media/allThatJazz-screenshot-artistInfo.b9104dad.png"
+  },
+  {
+    "revision": "a7c3bb973a7eff0c1e3a60dceff51696",
+    "url": "/static/media/allThatJazz-screenshot-homePage.a7c3bb97.png"
+  },
+  {
+    "revision": "004aae5c3721016b13dfc28295950f25",
+    "url": "/static/media/allThatJazz-screenshot-songsPage.004aae5c.png"
+  },
+  {
+    "revision": "413f66020fe57cd1b530266ff33fa595",
+    "url": "/static/media/app-icon-about.413f6602.png"
+  },
+  {
+    "revision": "78019826233b2ae960f8cfe8a3944115",
+    "url": "/static/media/app-icon-experience.78019826.jpg"
+  },
+  {
+    "revision": "a07b62e26de2c94f412baa55085a347b",
+    "url": "/static/media/app-icon-projects.a07b62e2.jpg"
+  },
+  {
+    "revision": "93b2c9d18aa4dca2f0b3db14af18a865",
+    "url": "/static/media/background-aboutPage.93b2c9d1.jpg"
+  },
+  {
+    "revision": "45a251acce6944175704e1a6f21c2855",
+    "url": "/static/media/background-homePage.45a251ac.gif"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "c87b240ef16305988a2c45fecc643543",
+    "url": "/static/media/powV1-screenshot-heroIndex.c87b240e.png"
+  },
+  {
+    "revision": "da5276ec0b450651535cfa43b4cf4adb",
+    "url": "/static/media/powV1-screenshot-heroShow.da5276ec.png"
+  },
+  {
+    "revision": "73f808d61f4206a8ad68c9e9b76df448",
+    "url": "/static/media/powV1-screenshot-homePage.73f808d6.png"
+  },
+  {
+    "revision": "8782530443b94bf62ad600d46da7af01",
+    "url": "/static/media/powV2-screenshot-heroBattle.87825304.gif"
+  },
+  {
+    "revision": "42d65010e6bd63f3f5e80057932cc63d",
+    "url": "/static/media/powV2-screenshot-homePage.42d65010.gif"
+  },
+  {
+    "revision": "8ac01ff7ef3180b24f941fd3a4895489",
+    "url": "/static/media/powV2-screenshot-markplacePage.8ac01ff7.gif"
+  },
+  {
+    "revision": "e6a9ecf7b27290d5212cc7ce37d5a54c",
+    "url": "/static/media/powV2-screenshot-profilePage.e6a9ecf7.png"
+  },
+  {
+    "revision": "071325f2540ce4ff0dd16a18562c43ce",
+    "url": "/static/media/spaceInvaders-screenshot-gameplay.071325f2.gif"
+  },
+  {
+    "revision": "b75c1c2153d840310f396e27b8fd6fda",
+    "url": "/static/media/spaceInvaders-screenshot-homepage.b75c1c21.png"
+  },
+  {
+    "revision": "d03f7b9f448b7b62cc1137fef127055e",
+    "url": "/static/media/spaceInvaders-screenshot-titlepage.d03f7b9f.png"
+  },
+  {
+    "revision": "0c2c17ef167b63e00337cc1df9236cab",
+    "url": "/static/media/theEarlyBird-screenshot-homePage.0c2c17ef.png"
+  },
+  {
+    "revision": "6f94089993b5f29c5c0ead60e79482cf",
+    "url": "/static/media/theEarlyBird-screenshot-profilePage.6f940899.gif"
+  },
+  {
+    "revision": "25d98ea183441a7f242a7e21964e5460",
+    "url": "/static/media/theEarlyBird-screenshot-publishersPage.25d98ea1.gif"
+  },
+  {
+    "revision": "f1fff187f8e9a45a64881b1ca1f68275",
+    "url": "/static/media/theEarlyBird-screenshot-topNewsPage.f1fff187.gif"
+  },
+  {
+    "revision": "8546b31bec1ffe7feda975f3d8238620",
+    "url": "/static/media/theEarlyBird-screenshot-weatherPage.8546b31b.png"
+  },
+  {
+    "revision": "7017ccd44fa9bce68644ac931e73b698",
+    "url": "/static/media/travelr-screenshot-homePage.7017ccd4.png"
+  },
+  {
+    "revision": "e98eb693df518746f404ab1137e9bdc3",
+    "url": "/static/media/travelr-screenshot-profilePage.e98eb693.png"
+  },
+  {
+    "revision": "c676f0586ea60acc8464b817e99d7bce",
+    "url": "/static/media/travelr-screenshot-tripPage.c676f058.png"
+  }
+]);
